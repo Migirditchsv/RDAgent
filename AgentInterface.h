5d2d0e9f144bb706e4e305aeb4a82a2c07ecafa8
Sam Migirditch <samigir@iu.edu>
2019-05-21 15:01:48 -0400

    Need to figure out how to make TVectors of structs.

--- AgentInterface.h
+++ AgentInterface.h
@@ -23,7 +23,8 @@
 //#include <cmath>
 
 // Headers
-#include "VectorMatrix.h"
+#include "VectorMatrix.h"// Duh
+#include "Perceptron.h"
 //#include "random.h"
 
 #pragma once
@@ -32,32 +33,51 @@
 // Public Globals
 // ****************************
 
-int inputperceptrons;// Number of input perceptrons
-int outputperceptrons;// Number of output perceptrons
-TVector<double> perceptronstates;// The state of the perceptron 
+//int inputperceptronnum;// Number of input perceptrons
+//int outputperceptronnum;// Number of output perceptrons
+//TVector<double> perceptronstates;// The state of the perceptron 
+
+// **************************** 
+// Objects
+// ****************************
+
+struct perceptron
+{
+    double state; // perceptron state = sum of weighted input(s)
+    TVector<int> source;
+    // ^ Potentially weighted source(s) a perceptron integrates
+    TVector<int> target;
+    //^ indicies of a TVector that weighted states are projected onto
+    TVector<int> channel;
+    // ^ The channel that is read from or two on a given controller channel
+    TVector<double> weight;
+    // ^ The weights that will be applied to integrated or projected values
+    // depending on if perceptron is input or output.
+};
 
 // **************************** 
 // RDControl Class Declaration
 // ****************************
 
-class PerceptronInterface
+class AgentInterface{
     public:
         // Constructor
-        PerceptronInterface(TVector<double> * environmentptr_,
-                            TMatrix<double> * controllerptr_,
-                            TVector<double> * actuatorptr_,
-                            int inperceptronnum_
-                            int outperceptronnum_,
-                            int MaxLinks_ // Number of in or out links for a perc
-                            )
-            {
+        AgentInterface(TVector<double> * eptr,
+                            TMatrix<double> * cptr,
+                            TVector<double> * aptr,
+                            int innum,
+                            int outnum,
+                            int maxnum, 
+                            int initnum){
+            
             // Safe copy arguments
-            environmentptr = environmentptr_;
-            controllerptr = controllerptr_;
-            actuatorptr = actuatorptr_;
-            inperceptronnum = inperceptronnum_;
-            outperceptronnum = outperceptronnum_;
-            maxlinks = maxlinks_;
+            environmentptr = eptr;
+            controllerptr = cptr;
+            actuatorptr = aptr;
+            inperceptronnum = innum;
+            outperceptronnum = outnum;
+            maxlinknum = maxnum;
+            initlinknum = initnum;
 
             // Pull in objects for warm up
             environment = &environmentptr;
@@ -67,31 +87,50 @@ class PerceptronInterface
             // Compute sizes for initialization
             environmentsize = environment.Size();
             controllersize  = controller.RowSize();
-            controllerdimension = controller.CollumnSize();
+            controllerdimension = controller.ColumnSize();
             actuatorsize = actuator.Size();
 
-            // Initialize arrays
-            
+            // Initialize Input Perceptrons
+            TVector<perceptron> inperceptrons;
+            inperceptrons.SetBounds(1,inperceptronnum);
+            inperceptrons.source.Setbounds(1,inperceptronnum);//single source
+            inperceptrons.target.SetBounds(1,maxlinknum);
+            inperceptrons.channel.SetBounds(1,maxlinknum);
+            inperceptrons.weight.SetBounds(1,maxlinknum);
 
-            }
+            // Initialize Output Perceptrons
+            outperceptrons.SetBounds(1,outperceptronnum);
+            };// end constructor
+
+
+
+            
     private:
     // **************************** 
     // Private Global Vars
     // ****************************
     
-    // Control and Safety
-    int maxlinks;
-    int environmentsize;
-    int controllersize;
+    // Array size control 
+    int inperceptronnum;// Number of input layer perceptrons
+    int outperceptronnum;// Number of output layer perceptrons
+    int maxlinknum;// Number of links to or from controller each perc starts with
+    int initlinknum;// Number of links to start with
+    int environmentsize;// Number of variables defining the sensable environment
+    int controllersize;// Number of objects composing the controller
     int controllerdimension; // number of channels in the controller
-    int actuatorsize;
+    int actuatorsize;// Number of actuators under agent control
+    
+    // Perceptron arrays
+    TVector<perceptron> inperceptrons;
+    TVector<perceptron> outperceptrons;
+
     // Pointers for linking
     TVector<double> * environmentptr;
     TMatrix<double> * controllerptr;    
     TVector<double> * actuatorptr;
     // Local copy of external states
     TVector<double>  environment;// on [0,1]
-    TMatrix<double>  controller;// weighted by [-1,1]
+    TMatrix<double>  controller;// on [0,1], to be weighted by [-1,1]
     TVector<double>  actuator;// on [0,1]
     // --------------------------The perceptrons-------------------------------
     // These arrays use the following convention for perceptron indicies:
@@ -111,11 +150,11 @@ class PerceptronInterface
     // done for out-layer perceptrons but for the target index instead.
     // Sorry, this was the cleanest way I could think to do it.
     // ------------------------------------------------------------------------
-    TMatrix<int> source;// The channels a perc pulls from
-    TMatrix<int> target;// The channels a perc projects onto
-    TMatrix<double> weight;// The weights applied to values pooled or projected
-    TMatrix<double> channel;
+    // TMatrix<int> source;// The channels a perc pulls from
+    // TMatrix<int> target;// The channels a perc projects onto
+    // TMatrix<double> weight;// The weights applied to values pooled or projected
+    // TMatrix<double> channel;
     // ^ The channel that an in-layer perc projects to for a given controller cell
-
     
     
+    }// end agentinterface class
